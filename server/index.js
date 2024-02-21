e4cd8edbe0c5ca0e630b969ad530bf1c3d713f4d
NITISH KUMAR <81707719+nitish-09105@users.noreply.github.com>
2024-02-21 23:30:38 +0530

    client

--- server/index.js
+++ server/index.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-
-
+const cors=require('cors');
 const app = express();
 
 // use express.json() to get data into JSON format
@@ -11,6 +10,9 @@ app.use(express.json());
 // PORT
 const PORT = process.env.PORT || 5500;
 
+// use cors
+app.use(cors());
+
 // import routes
 const TodoItemRoute = require('./routes/todoItems');
 
@@ -18,7 +20,7 @@ dotenv.config();
 // connect to mongoDB
 mongoose.connect(process.env.DB_CONNECT)
     .then(() => console.log('Database connected'))
-    .catch(err => console.log(err));
+.catch(err => console.log(err));
 
 app.use('/', TodoItemRoute);
 
