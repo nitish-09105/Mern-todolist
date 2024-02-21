afda3d932c94836ab1b6c3e5a368d3aba0dd3277
NITISH KUMAR <81707719+nitish-09105@users.noreply.github.com>
2024-02-21 22:16:14 +0530

    backend api creation

---
+++ server/index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+
+const app = express();
+
+// use express.json() to get data into JSON format
+app.use(express.json());
+
+// PORT
+const PORT = process.env.PORT || 5500;
+
+// import routes
+const TodoItemRoute = require('./routes/todoItems');
+
+dotenv.config();
+// connect to mongoDB
+mongoose.connect(process.env.DB_CONNECT)
+    .then(() => console.log('Database connected'))
+    .catch(err => console.log(err));
+
+app.use('/', TodoItemRoute);
+
+// add port and connect to server
+app.listen(PORT, () => console.log("Server connected"));
